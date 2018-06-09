c34f8e7b20015d76b49d46633a59d954db4c5128
weifei <912118614@qq.com>
2018-06-09 09:28:06 +0800

    ebay

--- src/main/java/com/commerceerp/system/controller/EBayController.java
+++ src/main/java/com/commerceerp/system/controller/EBayController.java
@@ -2,18 +2,13 @@ package com.commerceerp.system.controller;
 
 import com.commerceerp.system.service.EbayService;
 import com.commerceerp.system.util.ApiResult;
-import com.ebay.sdk.ApiContext;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Api(description = "ebay接口", value = "ebay接口")
